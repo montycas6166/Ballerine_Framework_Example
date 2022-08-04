f7b8c5446b3e282fa28f7f4adca933876f0c1144
Monty McCasland <momcas6166@gmail.com>
2022-08-04 19:06:20 +0500

    Webview integration to android

---
+++ lib/open_flow_page.dart
@@ -0,0 +1,53 @@
+import 'package:ballerina_framework_example/web_view_page.dart';
+import 'package:flutter/material.dart';
+
+class OpenFlowScreen extends StatefulWidget {
+  const OpenFlowScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OpenFlowScreen> createState() => _OpenFlowScreenState();
+}
+
+class _OpenFlowScreenState extends State<OpenFlowScreen> {
+  String? params;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: MediaQuery.of(context).padding,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  params = null;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (ctx) => WebViewScreen(
+                        onVerificationDone:
+                            (Map<String, List<String>> queryParameters) {
+                          params = queryParameters.toString();
+                          setState(() {});
+                        },
+                      ),
+                    ),
+                  );
+                },
+                child: const Text("Open Flow"),
+              ),
+              if (params != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(params ?? ""),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/web_view_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({Key? key, this.onVerificationDone}) : super(key: key);
+  final Function(Map<String, List<String>> queryParameters)? onVerificationDone;
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+  double progress = 0;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      useShouldOverrideUrlLoading: true,
+      mediaPlaybackRequiresUserGesture: false,
+      allowFileAccessFromFileURLs: true,
+      allowUniversalAccessFromFileURLs: true,
+    ),
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+      allowFileAccess: true,
+    ),
+    ios: IOSInAppWebViewOptions(
+      allowsInlineMediaPlayback: true,
+    ),
+  );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: MediaQuery.of(context).padding,
+        child: Stack(
+          children: [
+            InAppWebView(
+              key: webViewKey,
+              onWebViewCreated: (controller) {
+                webViewController = controller;
+                webViewController?.loadUrl(
+                  urlRequest: URLRequest(
+                    url: Uri.parse("https://vendy.dev.ballerine.app"),
+                  ),
+                );
+              },
+              onUpdateVisitedHistory: (controller, Uri? url, androidIsReload) async {
+                if (url.toString().contains("close")  ) {
+                  if (widget.onVerificationDone != null  ) {
+                    widget.onVerificationDone!(url!.queryParametersAll);
+                  }
+                  Navigator.pop(context);
+                }
+              },
+              onProgressChanged: (controller, progress) {
+                if (progress == 100) {}
+                setState(
+                  () {
+                    this.progress = progress / 100;
+                  },
+                );
+              },
+            ),
+            progress < 1.0
+                ? LinearProgressIndicator(value: progress)
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
